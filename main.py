479641eb3ad442b1e7725a2ee1234d86a6ea8671
Arya kesarwani <aryakesharwani1234@gmail.com>
2024-06-10 18:56:38 +0530

    Completed 3 testcases

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ def solve(N,K,categories,cooking_times):
         time=cooking_times[i]
         if category not in category_times or time < category_times[category]:
             category_times[category]=time
-        if len(category_times)<K:
-            return -1
-        total_time=sum(sorted(category_times.values())[:K])
-        return total_time
+    if len(category_times)<K:
+        return -1
+    total_time=sum(sorted(category_times.values())[:K])
+    return total_time
 
 
-test_case = int(input("enter the number of testcase "))
+T = int(input())
 
-for _ in range(test_case):
+for _ in range(T):
     N,K = map(int,input().split())
     categories=list(map(int,input().split()))
     cooking_times=list(map(int,input().split()))
     result = solve(N,K,categories,cooking_times)
-    print(result)
+    print("Minimum Time required to have the meal",result)
     
